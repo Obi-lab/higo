c82f766610460010fc8569b6124cef6c6d600960
Obi-lab <oballa99darel@gmail.com>
2023-09-06 17:46:03 +0300

    Initial development

---
+++ higo_ventures/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Homepage from './pages/Hompage'
+import Portfolio from './pages/Portfolio'
+import Landing from './pages/Landing'
+import Blog from './pages/Blog'
+import "./index.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=" h-screen items-center">
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/home' element={<Homepage/>} />
+        <Route path='/portfolio' element={<Portfolio/>} />
+        <Route path='/blog' element={<Blog/>} />
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
